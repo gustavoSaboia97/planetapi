6a199b39a707df89d462ec6730ab84907a20a306
Gustavo Saboia <gustavosaboia97@hotmail.com>
2019-02-21 01:09:40 -0300

    Created an UrlBuilder class to routes

---
+++ src/main/java/com/planetapi/components/UrlBuilder.java
@@ -0,0 +1,10 @@
+package com.planetapi.components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UrlBuilder {
+
+    public static final String PLANET_API_PATH = "/api/planet";
+    public static final String PLANET_API_PATH_NAME = "/name";
+}
