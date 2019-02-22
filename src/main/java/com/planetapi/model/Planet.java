6fa7a3e03396496ed31a7f937dcee40b4066ffb4
Gustavo Saboia <gustavosaboia97@hotmail.com>
2019-02-22 00:28:45 -0300

    Prepared Planet model to mongo

--- src/main/java/com/planetapi/model/Planet.java
+++ src/main/java/com/planetapi/model/Planet.java
@@ -2,11 +2,23 @@ package com.planetapi.model;
 
 import lombok.Data;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 @Data
+@Document
 public class Planet {
 
+    @Id
     private String id;
+
+    @Indexed(unique = true)
     private String name;
     private String climate;
     private String terrain;
+
+    @Transient
+    private int apparitionCounter;
 }
